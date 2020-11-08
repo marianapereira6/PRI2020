a4b1190d50a9607d053122f328edfcf5e749dcc1
Mariana Pereiras <mopereira98@gmail.com>
2020-11-08 23:50:44 +0000

    servidor corregido

--- TP4/server.js
+++ TP4/server.js
@@ -2,8 +2,10 @@ var http = require('http')
 var fs = require('fs')
 
 var servidor = http.createServer(function (req, res) {
-    if(req.url.match(/\/[1-122]$/)){
-        var num = req.url.split("/")[1]
+    
+    var num = req.url.split("/")[1]     
+
+    if(req.url.match(/\/[1-9]+$/)){
         console.log(num)
         fs.readFile('./xsl/site/arq'+num+'.html',function (err, data) {
             if(err){
@@ -18,15 +20,35 @@ var servidor = http.createServer(function (req, res) {
                 res.end()
             }
         })
+        
     }
     
-    else{
-        console.log("ERRO ficheiro não encontrado")
-        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
-        res.write("<p>Ficheiro inexistente.</p>")
-        res.end()
-    }
-})
+     else { console.log(num)
+            if (num === "index.html" || num === "" || num === "index"){
+                    console.log(num)
+                    fs.readFile('./xsl/site/index.html',function (err, data) {
+                        if(err){
+                            console.log("ERRO na leitura do ficheiro: " + err)
+                            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+                            res.write("<p>Ficheiro inexistente.</p>")
+                            res.end()
+                        }
+                        else{
+                            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+                            res.write(data)
+                            res.end()
+                        }
+                    })
+
+            }
+            else{
+                console.log("ERRO ficheiro não encontrado")
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+                res.write("<p>Ficheiro inexistente.</p>")
+                res.end()
+            }
+     }    
+    })
 
 servidor.listen(7777)
 console.log('Servidor à escuta na porta 7777')
